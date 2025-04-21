2ace2e5bec0cb39ec931e179a4ba2ab1029866b6
kosei <kosei.iidx@gmail.com>
2025-04-22 00:17:14 +0900

    add log bucket

--- global/s3.tf
+++ global/s3.tf
@@ -5,3 +5,21 @@ resource "aws_s3_bucket" "tfstate" {
     prevent_destroy = true
   }
 }
+
+resource "aws_s3_bucket" "log" {
+  bucket = "log-plan8"
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "log" {
+  bucket = aws_s3_bucket.log.id
+  rule {
+    id     = "expiration"
+    status = "Enabled"
+    expiration {
+      days = 7
+    }
+    filter {
+      prefix = ""
+    }
+  }
+}
